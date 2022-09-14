e74eb3f17f6ac672a23e1680627237b993aab7fa
Bowen LU <bowenlu.cloud@gmail.com>
2022-09-14 10:01:31 +0000

    thread demo

---
+++ 13MultiThread/ThreadDemo1.java
@@ -0,0 +1,13 @@
+public class ThreadDemo1 {
+    public static void main (String[] args) {
+        Thread t = new MyThread();
+        t.start();
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Start new thread");
+    }
+}
---
+++ 13MultiThread/ThreadDemo2.java
@@ -0,0 +1,13 @@
+public class ThreadDemo2 {
+    public static void main (String[] args) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+}
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Start my thread!");
+    }
+}
