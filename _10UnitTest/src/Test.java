68a6fefbb93253b26d8a931b559cf46300335566
CloudforJ <92019313+CloudforJ@users.noreply.github.com>
2022-09-17 17:21:42 +0800

    use junit to test

---
+++ _10UnitTest/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        if (Factorial.fact(10) == 3628800) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+    }
+}
